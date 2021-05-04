2287263c07655f168e0a2db34a677efd994831a2
CarlosHenrique <carlossouzap@outlook.com>
2021-05-04 10:55:38 -0300

    Add lógica de gerar frases e mudança de interface

--- app_frasesdodia/lib/main.dart
+++ app_frasesdodia/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,12 +15,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _frases = [
+    "Enfrente os problemas e leve a melhor!",
+    "Dê mais atenção ao que você tem de bom na sua vida.",
+    "Para chegar ao topo, o que importa é começar!",
+    "De nada adianta ter sonhos, se você não se empenhar em correr atrás.",
+    "Preste atenção nas oportunidades que estão à sua frente!",
+    "Positividade para começar o dia é colocar a sua fé em prática.",
+    "Não deixe nada nem ninguém estragar o seu bom humor.",
+    "Ontem já passou, é hora de planejar o futuro.",
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma nova frase!";
+
+  void _gerarFrase() {
+    var index = Random().nextInt(_frases.length);
+    setState(() {
+      _fraseGerada = _frases[index];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Frases do dia"),
-        backgroundColor: Colors.green,
+        title: Text("Daily Motivation"),
+        backgroundColor: Color(0xFF00BFA6),
       ),
       body: Center(
         child: Container(
@@ -30,10 +53,17 @@ class _HomeState extends State<Home> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Image.asset("images/logo.png"),
-              Text(
-                "Gerar uma frase.",
-                textAlign: TextAlign.justify,
-                style: TextStyle(fontSize: 25, color: Colors.grey),
+              Container(
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    color: Color(0xff00BFA6),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Text(
+                  _fraseGerada,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25, color: Colors.white),
+                ),
               ),
               RaisedButton(
                 child: Text(
@@ -43,8 +73,8 @@ class _HomeState extends State<Home> {
                       color: Colors.white,
                       fontWeight: FontWeight.bold),
                 ),
-                color: Colors.green[700],
-                onPressed: () {},
+                color: Color(0xFF00BFA6),
+                onPressed: _gerarFrase,
               )
             ],
           ),
